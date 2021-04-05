ae1f407e763a8f55ab5f27816b9360eca32c1458
Perova <perovaa.24@gmail.com>
2021-04-05 12:16:27 +0300

    feat: add alert for crosscheck with links to pages of animals

--- online-zoo/pages/index.js
+++ online-zoo/pages/index.js
@@ -1,5 +1,11 @@
 const TITLE = document.title;
 
+//temporarly
+alert("К сожалению я не успела выполнить ссылки-иконки для переход по страницам животных на странице zoos. \
+Чтобы посмотреть страницы других животных кроме панды используйте, пожалуйста следующие пути: \
+https://rolling-scopes-school.github.io/little-plumelet-JSFE2021Q1/online-zoo/pages/zoos-page-alligator/zoos-alligator.html \
+https://rolling-scopes-school.github.io/little-plumelet-JSFE2021Q1/online-zoo/pages/zoos-page-eagle/zoos-eagle.html")
+
 switch (TITLE) {
     case "Online-Zoo. Landing":
         document.querySelectorAll(".navigation__item").forEach(element => {
