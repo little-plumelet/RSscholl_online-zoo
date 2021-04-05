55f36f6bb4740942abb05590ea26333c0f2d95e4
Perova <perovaa.24@gmail.com>
2021-04-05 10:52:23 +0300

    feat: add js script to highlite active nav element

---
+++ online-zoo/pages/index.js
@@ -0,0 +1,30 @@
+const TITLE = document.title;
+
+switch (TITLE) {
+    case "Online-Zoo. Landing":
+        document.querySelectorAll(".navigation__item").forEach(element => {
+            element.classList.remove("active");
+        }); 
+        document.querySelectorAll(".navigation__item_about").forEach(element => {
+            element.classList.add("active");
+        });
+        break;
+
+    case "Online-Zoo. Map":
+        document.querySelectorAll(".navigation__item").forEach(element => {
+            element.classList.remove("active");
+        }); 
+        document.querySelectorAll(".navigation__item_map").forEach(element => {
+            element.classList.add("active");
+        });
+        break;
+
+    case "Online-Zoo. Panda":
+        document.querySelectorAll(".navigation__item").forEach(element => {
+            element.classList.remove("active");
+        }); 
+        document.querySelectorAll(".navigation__item_zoos").forEach(element => {
+            element.classList.add("active");
+        });
+        break;
+}
