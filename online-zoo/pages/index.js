eac0e1cdf7fb9e93ec01e5436ee8a370878f1ebd
Perova <perovaa.24@gmail.com>
2021-04-19 14:06:04 +0300

    fix: fix some responsive

--- online-zoo/pages/index.js
+++ online-zoo/pages/index.js
@@ -2,7 +2,7 @@ const TITLE = document.title;
 const SCCREEN_WIDTH = window.screen.width;
 
 //temporarly
-// alert("К сожалению я не успела выполнить ссылки-иконки для переход по страницам животных на странице zoos. \
+ alert("Прошу вас при проверке адаптива при растягивании окна между брейкпойнтами 320 640 и 1000 обновлять страницу. Я прикрутила изменения в DOM через JS но не знаю как привязать обращение к JS при растягивании окна");// \
 // Чтобы посмотреть страницы других животных кроме панды используйте, пожалуйста следующие пути: \
 // https://rolling-scopes-school.github.io/little-plumelet-JSFE2021Q1/online-zoo/pages/zoos-page-alligator/zoos-alligator.html \
 // https://rolling-scopes-school.github.io/little-plumelet-JSFE2021Q1/online-zoo/pages/zoos-page-eagle/zoos-eagle.html \
@@ -32,97 +32,96 @@ function zoos_page_320_mix_dom_elements () {
      
 }
 
+    switch (TITLE) {
+        case "Online-Zoo. Landing":
+            document.querySelectorAll(".navigation__item").forEach(element => {
+                element.classList.remove("active");
+            }); 
+            document.querySelectorAll(".navigation__item_about").forEach(element => {
+                element.classList.add("active");
+            });
+            break;
 
-switch (TITLE) {
-    case "Online-Zoo. Landing":
-        document.querySelectorAll(".navigation__item").forEach(element => {
-            element.classList.remove("active");
-        }); 
-        document.querySelectorAll(".navigation__item_about").forEach(element => {
-            element.classList.add("active");
-        });
-        break;
+        case "Online-Zoo. Map":
+            document.querySelectorAll(".navigation__item").forEach(element => {
+                element.classList.remove("active");
+            }); 
+            document.querySelectorAll(".navigation__item_map").forEach(element => {
+                element.classList.add("active");
+            });
+            break;
 
-    case "Online-Zoo. Map":
-        document.querySelectorAll(".navigation__item").forEach(element => {
-            element.classList.remove("active");
-        }); 
-        document.querySelectorAll(".navigation__item_map").forEach(element => {
-            element.classList.add("active");
-        });
-        break;
+        case "Online-Zoo. Panda":
+            document.querySelectorAll(".navigation__item").forEach(element => {
+                element.classList.remove("active");
+            });
+            document.querySelectorAll(".navigation__item_zoos").forEach(element => {
+                element.classList.add("active");
+            });
+            document.querySelectorAll(".cam-aside__icon").forEach(element => {
+                element.classList.remove("active");
+            });
+            document.getElementById("icon-panda").classList.add("active");
+            if (SCCREEN_WIDTH >= 640 && SCCREEN_WIDTH <=1599) {
+                zoos_page_1000_mix_dom_elements();
+            }
+            if (SCCREEN_WIDTH >= 320 && SCCREEN_WIDTH <=639) {
+                zoos_page_320_mix_dom_elements();
+            }
+            break;
 
-    case "Online-Zoo. Panda":
-        document.querySelectorAll(".navigation__item").forEach(element => {
-            element.classList.remove("active");
-        });
-        document.querySelectorAll(".navigation__item_zoos").forEach(element => {
-            element.classList.add("active");
-        });
-        document.querySelectorAll(".cam-aside__icon").forEach(element => {
-            element.classList.remove("active");
-        });
-        document.getElementById("icon-panda").classList.add("active");
-         if (SCCREEN_WIDTH >= 640 && SCCREEN_WIDTH <=1599) {
-             zoos_page_1000_mix_dom_elements();
-         }
-         if (SCCREEN_WIDTH >= 320 && SCCREEN_WIDTH <=639) {
-            zoos_page_320_mix_dom_elements();
-        }
-        break;
-
-    case "Online-Zoo. Eagle":
-        document.querySelectorAll(".navigation__item").forEach(element => {
-            element.classList.remove("active");
-        }); 
-        document.querySelectorAll(".navigation__item_zoos").forEach(element => {
-            element.classList.add("active");
-        });
-        document.querySelectorAll(".cam-aside__icon").forEach(element => {
-            element.classList.remove("active");
-        });
-        document.getElementById("icon-eagle").classList.add("active");
-        if (SCCREEN_WIDTH >= 640 && SCCREEN_WIDTH <=1599) {
-            zoos_page_1000_mix_dom_elements();
-        }
-        if (SCCREEN_WIDTH >= 320 && SCCREEN_WIDTH <=639) {
-            zoos_page_320_mix_dom_elements();
-        }
-        break;
-    case "Online-Zoo. Alligator":
-        document.querySelectorAll(".navigation__item").forEach(element => {
-            element.classList.remove("active");
-        }); 
-        document.querySelectorAll(".navigation__item_zoos").forEach(element => {
-            element.classList.add("active");
-        });
-        document.querySelectorAll(".cam-aside__icon").forEach(element => {
-            element.classList.remove("active");
-        });
-        document.getElementById("icon-alligator").classList.add("active");
-        if (SCCREEN_WIDTH >= 640 && SCCREEN_WIDTH <=1599) {
-            zoos_page_1000_mix_dom_elements();
-        }
-        if (SCCREEN_WIDTH >= 320 && SCCREEN_WIDTH <=639) {
-            zoos_page_320_mix_dom_elements();
-        }
-        break;
-    case "Online-Zoo. Gorilla":
-        document.querySelectorAll(".navigation__item").forEach(element => {
-            element.classList.remove("active");
-        }); 
-        document.querySelectorAll(".navigation__item_zoos").forEach(element => {
-            element.classList.add("active");
-        });
-        document.querySelectorAll(".cam-aside__icon").forEach(element => {
-            element.classList.remove("active");
-        });
-        document.getElementById("icon-gorilla").classList.add("active");
-        if (SCCREEN_WIDTH >= 640 && SCCREEN_WIDTH <=1599) {
-            zoos_page_1000_mix_dom_elements();
-        }
-        if (SCCREEN_WIDTH >= 320 && SCCREEN_WIDTH <=639) {
-            zoos_page_320_mix_dom_elements();
-        }
-        break;
-}
+        case "Online-Zoo. Eagle":
+            document.querySelectorAll(".navigation__item").forEach(element => {
+                element.classList.remove("active");
+            }); 
+            document.querySelectorAll(".navigation__item_zoos").forEach(element => {
+                element.classList.add("active");
+            });
+            document.querySelectorAll(".cam-aside__icon").forEach(element => {
+                element.classList.remove("active");
+            });
+            document.getElementById("icon-eagle").classList.add("active");
+            if (SCCREEN_WIDTH >= 640 && SCCREEN_WIDTH <=1599) {
+                zoos_page_1000_mix_dom_elements();
+            }
+            if (SCCREEN_WIDTH >= 320 && SCCREEN_WIDTH <=639) {
+                zoos_page_320_mix_dom_elements();
+            }
+            break;
+        case "Online-Zoo. Alligator":
+            document.querySelectorAll(".navigation__item").forEach(element => {
+                element.classList.remove("active");
+            }); 
+            document.querySelectorAll(".navigation__item_zoos").forEach(element => {
+                element.classList.add("active");
+            });
+            document.querySelectorAll(".cam-aside__icon").forEach(element => {
+                element.classList.remove("active");
+            });
+            document.getElementById("icon-alligator").classList.add("active");
+            if (SCCREEN_WIDTH >= 640 && SCCREEN_WIDTH <=1599) {
+                zoos_page_1000_mix_dom_elements();
+            }
+            if (SCCREEN_WIDTH >= 320 && SCCREEN_WIDTH <=639) {
+                zoos_page_320_mix_dom_elements();
+            }
+            break;
+        case "Online-Zoo. Gorilla":
+            document.querySelectorAll(".navigation__item").forEach(element => {
+                element.classList.remove("active");
+            }); 
+            document.querySelectorAll(".navigation__item_zoos").forEach(element => {
+                element.classList.add("active");
+            });
+            document.querySelectorAll(".cam-aside__icon").forEach(element => {
+                element.classList.remove("active");
+            });
+            document.getElementById("icon-gorilla").classList.add("active");
+            if (SCCREEN_WIDTH >= 640 && SCCREEN_WIDTH <=1599) {
+                zoos_page_1000_mix_dom_elements();
+            }
+            if (SCCREEN_WIDTH >= 320 && SCCREEN_WIDTH <=639) {
+                zoos_page_320_mix_dom_elements();
+            }
+            break;
+    }
